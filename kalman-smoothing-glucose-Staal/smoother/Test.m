8750565cfa9f03b4082e60630e3c262f99931440
sorush omisvar <Sorush.omidvar@tamu.edu>
2022-02-13 19:17:01 -0600

    Major:Kalman is added but the differences are minimal

--- kalman-smoothing-glucose-Staal/smoother/Test.m
+++ kalman-smoothing-glucose-Staal/smoother/Test.m
@@ -1,38 +1,45 @@
+clear all
+clc
 
-%addpath('.')
-opts = detectImportOptions('test.txt')
-opts = setvartype(opts,{'Glucose'},'double');
-table = readtable('test.txt',opts);
-y = table.Glucose;
-t = table.DateTime;
-smoother_result = SmoothSMBGData(t,y,'outlierRemoval',1,'dynamicModel',2);
-%Put smoother result into table as new column
-table.SmoothedGlucose = smoother_result.y_smoothed_at_tout;
-%To store table with smoothed glucose back to file, uncomment below
-%writetable(table,'test_smoothed.txt')
 
-%plots
-doPlotting=1;
-if doPlotting
-    figure()
-    plot(t,y,'r.','MarkerSize',20)
-    hold on
-    ol = smoother_result.outliers==1;
-    plot(t(ol),y(ol),'kx','MarkerSize',10)
-    plot(smoother_result.t_i,smoother_result.y_smoothed,'b-','LineWidth',2);
-    plot(smoother_result.t_i,smoother_result.y_smoothed+2*smoother_result.y_smoothed_sd,'b--');
-    plot(smoother_result.t_i,smoother_result.y_smoothed-2*smoother_result.y_smoothed_sd,'b--');
+cd '/Users/sorush/My Drive/Documents/Educational/TAMU/Research/Trial/Data/11-5-21-11-15-21/CGM'
+for counter= 1:1
+    participant="p"+string(counter)+"_fl.txt"
     
+    opts = detectImportOptions(participant)
+    opts = setvartype(opts,{'CGM'},'double');
+    table = readtable(participant,opts);
 
-    %Example with user-supplied tout
-    %t_i = t(1):seconds(1):t(end);
-    %smoother_result = SmoothSMBGData(t,y,'outlierRemoval',1,'dynamicModel',2,'tout',t_i);
-    %plot(t_i,smoother_result.y_smoothed_at_tout,'m-');
-
-    hold off
-    legend('Input glucose measurements','Outliers','Smoothed glucose','95% CI of estimate','location','NorthWest')
-end
-
-
+    y = table.CGM;
+    t = table.Time;
+    
+    smoother_result = SmoothSMBGData(t,y,'outlierRemoval',1,'dynamicModel',2);
+    %Put smoother result into table as new column
+    table.SmoothedGlucose = smoother_result.y_smoothed_at_tout;
+    %To store table with smoothed glucose back to file, uncomment below
+    writetable(table,"smoothed-"+participant)
+   
+    doPlotting=1;
+    if doPlotting
+        figure()
+        plot(t,y,'r.','MarkerSize',20)
+        hold on
+        ol = smoother_result.outliers==1;
+        plot(t(ol),y(ol),'kx','MarkerSize',10)
+        plot(smoother_result.t_i,smoother_result.y_smoothed,'b-','LineWidth',2);
+        plot(smoother_result.t_i,smoother_result.y_smoothed+2*smoother_result.y_smoothed_sd,'b--');
+        plot(smoother_result.t_i,smoother_result.y_smoothed-2*smoother_result.y_smoothed_sd,'b--');
+        
+    
+        %Example with user-supplied tout
+        t_i = t(1):seconds(1):t(end);
+        smoother_result = SmoothSMBGData(t,y,'outlierRemoval',1,'dynamicModel',2,'tout',t_i);
+        plot(t_i,smoother_result.y_smoothed_at_tout,'m-');
+    
+        hold off
+        legend('Input glucose measurements','Outliers','Smoothed glucose','95% CI of estimate','location','NorthWest')
+    end
+end    
+    
 
       
