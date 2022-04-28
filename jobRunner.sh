e4e9a6996d8b89aec6d847c18a9c460997c4f7a8
Sorush Omidvar <sorush.omidvar@cse-stmi-s2.engr.tamu.edu>
2022-04-28 16:43:03 -0500

    Major:finishing up every details

--- jobRunner.sh
+++ jobRunner.sh
@@ -1,16 +1,9 @@
-python3 trialProcessor.py 0 15
-python3 trialProcessor.py 0 20
-python3 trialProcessor.py 0 25
-python3 trialProcessor.py 0 30
-python3 trialProcessor.py 0 35
-python3 trialProcessor.py 0 40
-python3 trialProcessor.py 0 45
-python3 trialProcessor.py 0 50
-python3 trialProcessor.py 0 55
-python3 trialProcessor.py 0 60
-python3 trialProcessor.py 0 65
-python3 trialProcessor.py 0 70
-python3 trialProcessor.py 0 75
-python3 trialProcessor.py 0 80
-python3 trialProcessor.py 0 85
-python3 trialProcessor.py 0 90
+python3 mamad.py 30
+python3 mamad.py 45
+python3 mamad.py 60
+python3 mamad.py 90
+python3 mamad.py 120
+python3 mamad.py 150
+python3 mamad.py 180
+python3 mamad.py 240
+python3 mamad.py 300
