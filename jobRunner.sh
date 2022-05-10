97939468be9bde188646feb149c4e42a19c15ea1
Sorush Omidvar <sorush.omidvar@cse-stmi-s2.engr.tamu.edu>
2022-05-09 20:50:09 -0500

    Major:Finalizing the code and commenting all components

--- jobRunner.sh
+++ jobRunner.sh
@@ -1,9 +1,11 @@
-python3 mamad.py 30
-python3 mamad.py 45
-python3 mamad.py 60
-python3 mamad.py 90
-python3 mamad.py 120
-python3 mamad.py 150
-python3 mamad.py 180
-python3 mamad.py 240
-python3 mamad.py 300
+python3 mamad.py 30 30
+python3 mamad.py 45 30
+python3 mamad.py 60 30
+python3 mamad.py 90 30
+
+python3 mamad.py 30 30
+python3 mamad.py 30 45
+python3 mamad.py 30 60
+python3 mamad.py 30 75
+python3 mamad.py 30 90
+python3 mamad.py 30 120
